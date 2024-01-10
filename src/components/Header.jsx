f167f64466e5e3c9fad5c8bc9759ee10a16800e7
Bienfait1992 <georgesakake@gmail.com>
2024-01-10 15:21:56 +0000

    I added a timeline class to Outlet to give it borders

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ function Header(){
     return(
 <>
 <div className="header">
-    <h1 className="page-title">Home home home</h1>
+    <h1 className="page-title">Home</h1>
     <img src="src/images/Top-Tweets.png" className="top-tweets"/>
 </div>      
 </>
--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -8,7 +8,7 @@ function Layout() {
     return (
         <>
             <LeftSidebar />  
-      <Outlet />
+      <div className='timeline'><Outlet /></div>
       <RightSidebar />
         </>
     );
