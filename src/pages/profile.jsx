67d39bcfc8b3564fc9364b6451ef44764b38486b
Akake <georges.akake@iMac-de-kadea-105.local>
2024-01-25 16:06:03 +0100

    le hover sur les icones

--- src/pages/profile.jsx
+++ src/pages/profile.jsx
@@ -1,7 +1,14 @@
 import React from "react";
-export default function Profile(){
-    return(
-        <h1>Profile</h1>
+export default function Profile() {
+    return (
+        <>
+            <div >
+                <div className="timeline"><img src="src/images/icone/1500x500.jpeg" alt="" className="imagemur" /></div>
+                <div><img src="src/images/icone/k5ytMjHq_400x400 copy.jpg" alt="" className="imageprofile" /></div>
+            </div>
+
+
+
+        </>
     )
 }
- 
